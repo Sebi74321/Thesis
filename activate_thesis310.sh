d39b55cceb477fa26e68067da4ce0a7bb58803c2
default <default@maastrichtuniversity.nl>
2026-07-21 15:12:42 +0200

    setup script

---
+++ activate_thesis310.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+unset PYTHONPATH
+unset PYTHONHOME
+unset LD_PRELOAD
+unset LD_LIBRARY_PATH
+
+export CONDA_PREFIX="/workspace/tmp_micromamba/root/envs/thesis310"
+export PATH="$CONDA_PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$CONDA_PREFIX/lib/python3.10/site-packages/torch/lib"
